03071801cfd34207b31006f885e804ee3d5be326
Matt <mattvroom1@gmail.com>
2018-03-26 18:53:41 -0400

    Update 2, animations

--- app.js
+++ app.js
@@ -13,6 +13,26 @@ for (i = 0; i < acc.length; i++) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+    }
   });
+}
+
+//Modal for Images
+
+let modal = document.getElementById('myModal');
+
+let img = $('.myImg');
+let modalImg = $("#img01");
+let captionText = document.getElementById("caption");
+$('.myImg').click(function(){
+    modal.style.display = "block";
+    let newSrc = this.src;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = this.alt;
+});
+
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
 }
